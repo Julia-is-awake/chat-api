4c8dff563d483171a35529a10840bcbfcb248648
julia-is-awake <juliasorgetzalves@gmail.com>
2024-09-04 14:50:57 -0300

    ajeitando cors

--- src/api.js
+++ src/api.js
@@ -13,6 +13,17 @@ var cors = require("cors");
 
 app.use(cors());
 
+router.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+});
+
 app.use(
   "/",
   router.get("/", (req, res) => {
